d9c2fddfcac1c6379e7bd252fc0b552cd06d0b30
Sahill131 <sahil2007tiwari@gmail.com>
2025-12-29 21:09:54 +0530

    change the hero section

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.to(".scroller",{
+    x:"-280%",
+    scrollTrigger:{
+        trigger:".scroller",
+        scroller:"body",
+        start:"top 30%",
+        end:"top -100%",
+        scrub:5,
+        pin:true
+        
+
+    }
+})
+gsap.to(".uii h1",{
+    x:"-200%",
+    repeat:-1,
+    duration:8,
+
+
+    ease: "none"
+
+    
+})
